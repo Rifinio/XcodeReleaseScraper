fdc6d13aa6e8986a2ae8e83d6235c987897b39f6
Adil Bougamza <rifinio88@gmail.com>
2018-04-19 00:23:08 +0200

    update Readme

--- scrapy.py
+++ scrapy.py
@@ -1,11 +1,16 @@
 # import libraries
 import urllib2
 from bs4 import BeautifulSoup
+import os
+import requests
+import json
 
 # Variables
 url = 'https://developer.apple.com/news/releases/'
-word_to_look_for = 'XCode'
-latest_release_version = '9.3'
+word_to_look_for = os.getenv('APPLE_KEYWORD', 'XCode')
+latest_release_version = os.getenv('APPLE_VERSION', '9.3') 
+channels = os.getenv('CHANNELS', 'slack-cahnnel-name').split(',') 
+WEBHOOK_URL = os.getenv('WEBHOOK_URL') #use your own webhook url, in this case it's using slack
 
 # Load the page
 page = urllib2.urlopen(url)
@@ -34,7 +39,14 @@ new_release_version = 0
 if len(release_data_split) > 0:
 	new_release_version = float(release_data_split[1])
 
+
+message = ''
 if new_release_version > float(latest_release_version):
-	print("Found it !", last_news_release)
-else: 
-	print("No new releases for " + word_to_look_for)
+	message = 'A new '+ word_to_look_for +' release is out :rocket:' + '\n:pizza_party: '+last_news_release + ' :pizza_party: \nFind out more here ' + url
+	for channel in channels:
+		response = requests.post(WEBHOOK_URL, data=json.dumps({"text" : message, "channel": "#"+ channel}), headers={'Content-Type': 'application/json'})
+		print (response.text)
+else:
+	message = "No new releases for " + word_to_look_for
+
+print(message)
