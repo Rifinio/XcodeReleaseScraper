f9da84d41e7ea69c43e822e705dcd718e08cd1cf
Adil Bougamza <rifinio88@gmail.com>
2018-04-19 00:10:35 +0200

    script that scrapes the apple release page and prints if new version is out

--- xcode-scrapy.py
+++ xcode-scrapy.py
@@ -2,18 +2,39 @@
 import urllib2
 from bs4 import BeautifulSoup
 
-quote_page = 'http://www.bloomberg.com/quote/SPX:IND'
+# Variables
+url = 'https://developer.apple.com/news/releases/'
+word_to_look_for = 'XCode'
+latest_release_version = '9.3'
 
-page = urllib2.urlopen(quote_page)
-
+# Load the page
+page = urllib2.urlopen(url)
 soup = BeautifulSoup(page, 'html.parser')
 
-# Take out the <div> of name and get its value
-name_box = soup.find('h1', attrs={'class': 'name'})
-name = name_box.text.strip() # strip() is used to remove starting and trailing
-print name
+# Get a list of new releases
+news_boxes = soup.findAll('a', attrs={'class': 'article-title'})
 
-# get the index price
-price_box = soup.find('div', attrs={'class':'price'})
-price = price_box.text
-print price
+# Loop through the news and append titles to a list
+news_titles = []
+for box in news_boxes:
+	news = box.text.strip()
+	news_titles.append(news)
+
+# Find the latest xcode in the list of releases
+last_news_release = ''
+for title in news_titles:
+	if word_to_look_for.upper() in title.upper():
+		last_news_release = title
+		break
+
+# Check if a new release is out
+release_data_split = last_news_release.split()
+new_release_version = 0
+
+if len(release_data_split) > 0:
+	new_release_version = float(release_data_split[1])
+
+if new_release_version > float(latest_release_version):
+	print("Found it !", last_news_release)
+else: 
+	print("No new releases for " + word_to_look_for)
